fc1308a134aab6d1419eca32783757f43e69a7c7
sabuj.das <sabuj.das@8bf39cae-3674-11de-9e80-b52b74bdfc52>
2010-12-10 12:03:13 +0000

    Reading-Pad added

---
+++ trunk/reading-pad/rpad-installer/conf/docs/Readme.txt
@@ -0,0 +1,9 @@
+Reading Pad
+
+Is a free software for reading different types of e-Books. This software mainly supports the following types of e-Books: 
+
+PDB / PRC - container format for code databases in Palm OS, Garnet OS and Access Linux Platform. 
+PDF - Portable Document Format 
+TXT / TEXT - Simple text file 
+CBR / CBZ / RAR - Comic book files 
+
---
+++ trunk/reading-pad/rpad/rpad-launcher/src/main/java/com/gs/rpad/launcher/ReadingPadLauncher.java
@@ -0,0 +1,44 @@
+/** *****************************************************************************
+ * 		Reading Pad																*
+ *  Is a free software for reading different types of e-Books.					*
+ * 	This software mainy supports the following types of e-Books:				*
+ * 		1. PDB / PRC - container format for code databases in Palm OS, 			*
+ * 			Garnet OS and Access Linux Platform. 								*
+ * 		2. PDF - Portable Document Format										*
+ * 		3. TXT / TEXT - Simple text file										*
+ * 		4. CBR / CBZ / RAR - Comic book files									*
+ * 																				*
+ * 																				*
+ * ---  File Info  -------------------------------------------------------------*
+ * 
+ * 	Name	: ReadingPadLauncher.java
+ * 	Type	: com.gs.rpad.launcher.ReadingPadLauncher
+ * 
+ * 	Created	: Dec 10, 2010
+ * 	
+ * 	Author	: Sabuj Das [ mailto::sabuj.das@gmail.com ]
+ * 
+ * -----------------------------------------------------------------------------*
+ * 																				*
+ * Copyright © Sabuj Das 2010 All Rights Reserved. 								*
+ * <br/>No part of this document may be reproduced without written 				*
+ * consent from the author.														*
+ * 																				*
+ ****************************************************************************** */
+
+package com.gs.rpad.launcher;
+
+/**
+ * This class is the entry point of the application.
+ * This class contains the main() method.
+ * 
+ * 
+ * Before launching the reading pad, it will create all the 
+ * initial setup required for the system.
+ *
+ */
+public class ReadingPadLauncher {
+
+	
+	
+}
