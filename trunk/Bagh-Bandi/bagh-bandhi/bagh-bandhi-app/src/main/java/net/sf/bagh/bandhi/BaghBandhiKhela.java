3794f34ba3a9b938283d8043fcfc1f0c970688cf
sabuj.das <sabuj.das@8bf39cae-3674-11de-9e80-b52b74bdfc52>
2012-11-10 04:10:17 +0000

    Game frame added

--- trunk/Bagh-Bandi/bagh-bandhi/bagh-bandhi-app/src/main/java/net/sf/bagh/bandhi/BaghBandhiKhela.java
+++ trunk/Bagh-Bandi/bagh-bandhi/bagh-bandhi-app/src/main/java/net/sf/bagh/bandhi/BaghBandhiKhela.java
@@ -9,6 +9,7 @@ import java.awt.Point;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
+import net.sf.bagh.bandhi.app.GameFrame;
 
 import net.sf.bagh.bandhi.app.board.BoardBasePanel;
 import net.sf.bagh.bandhi.app.board.UiBox;
@@ -23,12 +24,39 @@ public class BaghBandhiKhela {
 	public static final int FRAME_HEIGHT = 730;
 	
 	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		new TestFrame().setVisible(true);
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(GameFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(GameFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(GameFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GameFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
 
-	}
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new GameFrame().setVisible(true);
+            }
+        });
+    }
 
 }
 
