ac2cbfa2d5536aa279ee181d78d8904fc7057802
sabuj.das <sabuj.das@8bf39cae-3674-11de-9e80-b52b74bdfc52>
2013-07-16 15:22:29 +0000

    ETL Job Manager

--- Job-Manager/etl-mgr/etl-mgr-apps/etl-mgr-web/src/main/java/com/mercuria/etl/mgr/client/EtlManager.java
+++ Job-Manager/etl-mgr/etl-mgr-apps/etl-mgr-web/src/main/java/com/mercuria/etl/mgr/client/EtlManager.java
@@ -1,5 +1,7 @@
 package com.mercuria.etl.mgr.client;
 
+import java.util.logging.Logger;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -10,6 +12,8 @@ import com.mercuria.etl.mgr.client.i18n.ApplicationMessages;
  */
 public class EtlManager implements EntryPoint {
 
+	private static Logger logger = Logger.getLogger(EtlManager.class.getName());
+	
 	public static final ApplicationMessages MESSAGES = GWT
 			.create(ApplicationMessages.class);
 
@@ -17,6 +21,7 @@ public class EtlManager implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		logger.info("In EtlManager.onModuleLoad()");
 		AppController appViewer = new AppController();
 	    appViewer.go(RootPanel.get());
 	}
