5f2ee665e2838da2e5b04744fb618bf8dac95f58
sabuj.das <sabuj.das@8bf39cae-3674-11de-9e80-b52b74bdfc52>
2014-07-01 01:16:11 +0000

    Added Editorial

---
+++ trunk/Tools/Editorial/editorial/editorial-launcher/src/main/java/com/gs/tools/editorial/launcher/EditorialLauncher.java
@@ -0,0 +1,11 @@
+package com.gs.tools.editorial.launcher;
+
+/**
+ * Launches the Editorial application. <br/>Load Initial Context. <br/>Do Housekeeping on
+ * exit.
+ */
+public class EditorialLauncher {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
